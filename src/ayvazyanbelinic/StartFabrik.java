a560ce70b6fe3b152d427831dc0e78ef4f6789dd
aayvazyan-tgm <ariay@live.at>
2013-09-30 00:34:11 +0200

    Mylogger entfernt; Startet nun fabrik, log4J eingebunden

--- src/ayvazyanbelinic/StartFabrik.java
+++ src/ayvazyanbelinic/StartFabrik.java
@@ -1,36 +1,39 @@
 package ayvazyanbelinic;
 
-import org.apache.commons.cli2.OptionException;
+import java.io.File;
 
+import org.apache.commons.cli2.OptionException;
+import org.apache.log4j.*;
 
 /**
  * Startet die Fabrik
- * @author Vennesa Belinic
- * @version 2013-09-26
+ * @author  Ari Ayvazyan ,Vennesa Belinic
+ * @version 2013-09-29
  */
 public class StartFabrik {
 
-	//Attribut(e)
 	
-	/**
-	 * 
-	 */
-	private static MyLogger logger;
-	
-	
-	
-	//Methode(n)
 	
 	/**
 	 * main-Methode (Startet alles)
-	 * @param args die Argumente die vom CLI Ã¼bergeben werden
+	 * @param args die Argumente die vom CLI uebergeben werden
 	 */
 	public static void main(String[] args) {
-		try {	
-			new MyCommandLine(args);
+		try {
+			MyCommandLine m=new MyCommandLine(args);//Verarbeitet die argumente
+			
+			FileAppender ap=new FileAppender(); //ein appender wird für die Log Datei erstellt
+			ap.setFile(m.getLogVerzeichnis()); //die Datei fuer den appender wird gesetzt
+			BasicConfigurator.configure(ap);  //der appender wird dem logger zugewiesen
+			Logger lg=Logger.getLogger("Arbeitsverlauf"); //Im Arbeitsverlauf finden sich saemtliche aktivitaeten innerhalb der Fabrik
+			
+			lg.log(Level.INFO, "Der Betrieb wird aufgenommen");
+			new Fabrik(m.getLagerVerzeichnis(), m.getLieferantenAnzahl(), m.getMonteurAnzahl(), m.getLagerMitarbeiterAnzahl(), m.getLaufzeit());
+			lg.log(Level.INFO, "Der Betrieb wurde aufgenommen");
+			
 		} catch(OptionException e) {
 			System.out.println("Es ist ein Fehler beim Verarbeiten aufgetreten!\n" +
-					"Entweder haben Sie nicht alle verpflichtenden Optionen und deren Argument angegeben,\n" +
+					"Entweder sind nicht alle verpflichtenden Optionen und deren Argument angegeben,\n" +
 					"oder es ist ein allgmeiner Fehler aufgetreten.");
 		} catch(IllegalArgumentException e) {
 			System.out.println(e.getMessage());
