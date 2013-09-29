1d33b8b569b711da0fdb4a07e6c4cec65d7f7ac7
aayvazyan-tgm <ariay@live.at>
2013-09-29 19:07:55 +0200

    umlaut in den kommentaren entfernt

--- src/ayvazyanbelinic/Stoppable.java
+++ src/ayvazyanbelinic/Stoppable.java
@@ -12,7 +12,7 @@ public interface Stoppable extends Runnable {
 	//Methodenko(e)pf(e)
 	
 	/**
-	 * stop() soll die implementierende klasse am weiterem ausführen hindern
+	 * stop() soll die implementierende klasse am weiterem ausfuehren hindern
 	 */
 	public abstract void stop();
 
