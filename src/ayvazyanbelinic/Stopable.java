354adcab94f654c02c2206ec08905af7bd88c077
aayvazyan-tgm <ariay@live.at>
2013-09-29 18:58:08 +0200

    Author & Javadoc hinzugefügt

--- src/ayvazyanbelinic/Stopable.java
+++ src/ayvazyanbelinic/Stopable.java
@@ -3,7 +3,7 @@ package ayvazyanbelinic;
 
 /**
  * 
- * @author User
+ * @author Ari Ayvazyan
  *
  */
 public interface Stopable extends Runnable {
@@ -12,7 +12,7 @@ public interface Stopable extends Runnable {
 	//Methodenko(e)pf(e)
 	
 	/**
-	 * 
+	 * stop() soll die implementierende klasse am weiterem ausführen hindern
 	 */
 	public abstract void stop();
 
