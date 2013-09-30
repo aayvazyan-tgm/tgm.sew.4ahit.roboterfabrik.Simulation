c390152b615200f01b94f15fec2d9f3189988668
Ari <ariay@live.at>
2013-09-30 14:57:13 +0200

    liefern() unvollstaendig, sektretariat hinzugefuegt

--- src/ayvazyanbelinic/Lieferant.java
+++ src/ayvazyanbelinic/Lieferant.java
@@ -3,69 +3,132 @@ package ayvazyanbelinic;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Random;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 
 /**
  * 
- * @author User
+ * @author Ari Ayvazyan
+ * @version 30.09.2013
  *
  */
 public class Lieferant implements Stoppable {
 
 	
-	//Attribut(e)
 	
 	/**
-	 * 
+	 * der Seed fuer die Zufallszahlen der lieferungen
 	 */
 	private long seed;
 
 	/**
-	 * 
+	 * Die ID des Mitarbeiters
 	 */
 	private LinkedList<Long> id;
 	
-	
-	
-	//Konstruktor(en)
+	/**
+	 * Mit stop() zu setzen, dient um die weitere ausfuehrung zu stoppen.
+	 */
+	private boolean stop=false;
 	
 	/**
-	 * 
-	 * @param id
-	 * @param lagerverzeichnis
-	 * @param lagerM
+	 *  Der Lagermitarbeiter
 	 */
-	public Lieferant(LinkedList<Long> id, String lagerverzeichnis, LagerMitarbeiter lagerM) {
-		
+	private LagerMitarbeiter lagerM;
+	
+	/**
+	 * das Lagerverzeichnis
+	 */
+	private String lagerVerz;
+
+	/**
+	 * das sekretariat fuer die ID vergabe
+	 */
+	private Sekretariat sekretariat;
+	
+	/**
+	 * Erstellt einen neuen Lieferanten der neue Teile in den angegebenen pfad liefert.
+	 * 
+	 * @param id - die ID des Lieferanten
+	 * @param lagerverzeichnis - Das zu benutzende Verzeichnis
+	 * @param lagerM - Der zu benutzende LagerMitarbeiter
+	 * @param sekretariat - fuer die Bauteil ID vergabe
+	 */
+	public Lieferant(LinkedList<Long> id, String lagerVerzeichnis, LagerMitarbeiter lagerM,Sekretariat sekretariat) {
+		this.id=id;
+		this.seed=1337l;
+		this.lagerM=lagerM;
+		this.lagerVerz=lagerVerzeichnis;
+		this.sekretariat=sekretariat;
 	}
 	
 	
 	
 	
-	//Methode(n)
 	
 	/**
 	 * 
-	 * @param bestandteilname
+	 * @param bestandteilname - Kann Arm, Rumpf, Auge oder Kettenantrieb sein.
 	 */
 	protected void liefern(String bestandteilname) {
+		Logger logger=Logger.getLogger("Arbeitsverlauf");
+		
+		if(bestandteilname.equalsIgnoreCase("Arm")){
+			Arm bestandteil=new Arm(this.sekretariat.getBauTeilID()); //erstelle einen neuen Arm mit der vom Sektretariat vergebenen ID
+			logger.log(Level.INFO, "Neues Teil an einen Lagermitarbeiter geliefert:"+bestandteil.toString());
+			//lagerM.  //Liefere das erstellte teil aus.
 
+		}
 	}
 
 	/**
 	 * @see Stoppable#stop()
 	 */
-	public void stop() {
-
+	public void stop(){ 
+		this.stop=true;
 	}
 
 	/**
-	 * 
+	 * @see Runnable#run()
 	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		Random r=new Random(seed);
+		long arbeitsgeschwindigkeit=100l; //in Millisekunden
 		
+		while(!stop){//Liefere solange bis stop() aufgerufen wird
+			try {
+				Thread.sleep(arbeitsgeschwindigkeit);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			int bestandTeilZfZahl=r.nextInt(4); //Liefert einen pseudo zufaelligen wert um ein zufaelliges Bestandteil zu erstellen.
+			switch(bestandTeilZfZahl){
+			case 0:
+				liefern("Auge");
+				liefern("Auge");
+				break;
+			
+			case 1:
+				liefern("Rumpf");
+				break;
+			
+			case 2:
+				liefern("Kettenantrieb");
+				break;
+			
+			case 3:
+				liefern("Arm");
+				liefern("Arm");
+				break;
+				
+			default: //sollte nie auftreten
+				System.err.println("Die Zufallszahl liegt ausserhalb der erwarteten Werte. Klassenname: "+getClass().getName());
+			}
+		}
 	}
 
 }
