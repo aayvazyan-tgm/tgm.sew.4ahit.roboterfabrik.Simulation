5692a737852f333a14299e56bd515a93558a112c
Ari <ariay@live.at>
2013-10-01 13:18:21 +0200

    Schreiben in Daten ausgelagert

--- src/ayvazyanbelinic/LagerMitarbeiter.java
+++ src/ayvazyanbelinic/LagerMitarbeiter.java
@@ -13,6 +13,7 @@ import java.util.RandomAccess;
 import java.util.Scanner;
 
 import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 
 /**
@@ -51,75 +52,84 @@ public class LagerMitarbeiter{
 	/**
 	 * @param bestandteilname gibt ein Bestandteil des gewuenschten typs zurueck. Auge, Kettenantrieb, Rumpf, Arm
 	 *
-	 * @return das angeforderte Bestandteil, kann mit instanceof zugeordnet werden. Gibt null zurueck wenn das angeforderte Teil nicht vorhanden ist, oder ein 
+	 * @return das angeforderte Bestandteil, kann mit instanceof zugeordnet werden. Gibt null zurueck wenn das angeforderte Teil nicht vorhanden ist, oder ein fehler aufgetreten ist.
 	 */
 	public synchronized Bestandteil getBestandteil(String bestandteilname) {
 		if(bestandteilname.equalsIgnoreCase("Arm")){
 			File f=new File(lagerVerzeichnis+File.pathSeparator+"arme.csv");
-			try{
-				Scanner fileScanner = new Scanner(f);
-				if(!fileScanner.hasNext()){
-					fileScanner.close(); //filescanner vor dem return wieder freigeben
-					return null;
-				}
-				String eingelesen=fileScanner.nextLine(); //Die gelesene Zeile
-				FileWriter fileStream = new FileWriter(f);
-				BufferedWriter out = new BufferedWriter(fileStream);
-				while(fileScanner.hasNextLine()) { //schreibe die Datei ohne den bestandteil neu
-				    String next = fileScanner.nextLine();
-				    if(next.equals("\n")) out.newLine();
-				    else out.write(next);
-				    out.newLine();   
-				}
-				out.close(); //ressourcen freigeben
-				fileScanner.close();
-				fileStream.close();
-				
-				return Bestandteil.getBestandTeil(eingelesen); //wandelt den String aus dem file in ein Bestandteil objekt um.
-	        } catch (FileNotFoundException e){
-	            System.out.println("Error: "+e);
-	        } catch (IOException e)
-	        {
-	            System.out.println("Error: "+e);
-	        }
+			return getBestandTeilvonFile(f);
 		}
 		if(bestandteilname.equalsIgnoreCase("Rumpf")){
 			File f=new File(lagerVerzeichnis+File.pathSeparator+"rumpf.csv");
+			return getBestandTeilvonFile(f);
 		}
 		if(bestandteilname.equalsIgnoreCase("Auge")){
 			File f=new File(lagerVerzeichnis+File.pathSeparator+"augen.csv");
+			return getBestandTeilvonFile(f);
 		}
 		if(bestandteilname.equalsIgnoreCase("Kettenantrieb")){
 			File f=new File(lagerVerzeichnis+File.pathSeparator+"kettenantrieb.csv");
+			return getBestandTeilvonFile(f);
 		}
-		return null;
+		return null; //gibt null zurueck falls der bestandteilname nicht gefunden wird
 	}
-	
+	/**
+	 * nimmt sich aus dem gegebenem pfad ein bestandteil, entfernt ihn aus der Datei und gibt ihn als parameter zurueck. 
+	 * Tritt ein fehler auf oder ist kein eintrag vorhanden wird null zurueckgegeben.
+	 * @param f - ist der pfad aus dem der Bestandteil hergeleitet werden soll
+	 * @return gibt den Bestandteil aus dem pfad zurueck
+	 */
+	private static Bestandteil getBestandTeilvonFile(File f) {
+		try{
+			Scanner fileScanner = new Scanner(f);
+			if(!fileScanner.hasNext()){ //Sollte kein bestandteil vorhanden sein wird null zurueckgegeben
+				fileScanner.close(); //filescanner vor dem return wieder freigeben
+				return null;
+			}
+			String eingelesen=fileScanner.nextLine(); //Die gelesene Zeile
+			FileWriter fileStream = new FileWriter(f);
+			BufferedWriter out = new BufferedWriter(fileStream);
+			while(fileScanner.hasNextLine()) { //schreibe die Datei ohne den bestandteil neu
+			    String next = fileScanner.nextLine();
+			    if(next.equals("\n")) out.newLine();
+			    else out.write(next);
+			    out.newLine();   
+			}
+			out.close(); //ressourcen freigeben
+			fileScanner.close();
+			fileStream.close();
+			
+			return Bestandteil.getBestandTeil(eingelesen); //wandelt den String aus dem file in ein Bestandteil objekt um.
+        } catch (FileNotFoundException e){
+            System.out.println("Error: "+e);
+        } catch (Exception e)
+        {
+            System.out.println("Error: "+e);
+        }
+		return null; //bei auftreten eines fehlers wird null zurueckgegeben
+	}
+
+
+
+
+
 	/**
 	 * 
-	 * @param bestandteil
-	 * @return
+	 * @param bestandteil das einzulagernde Bestandteil
+	 * @return 
 	 */
-	public boolean zurueckgeben(Bestandteil bestandteil) {		//wird benötigt um angeforderte Bauteile zurückzugeben falls sie nicht brauchbar sind
-		return true;
-	}
-	
-	/**
-	 * 
-	 * @param bestandteil
-	 * @return
-	 */
-	public boolean liefern(Bestandteil bestandteil) {		//wird benötigt um Bestanteile zu liefern (Liefrerant)
+	public synchronized boolean einlagern(Bestandteil bestandteil) {
+		Logger logger=Logger.getLogger("Arbeitsverlauf");
+		logger.log(Level.INFO, "Bestandteil eingelagert: "+bestandteil.toString());
 		return true;
 	}
 	
 	/**
 	 * 
 	 * @param roboter
-	 * @param monteur
 	 * @return
 	 */
-	public boolean liefern(Threadee roboter, Monteur monteur) {		//wird benötigt um fertig Roboter zu liefern (Monteur)
+	public boolean einlagern(Threadee roboter) {
 		return true;
 	}
 
